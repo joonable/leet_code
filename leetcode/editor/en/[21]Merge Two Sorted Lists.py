11986cde55e57daacc2390d0c49210d64c352d30
joon <joonable2@gmail.com>
2023-11-30 13:09:46 +0000

    initial commit

--- leetcode/editor/en/[21]Merge Two Sorted Lists.py
+++ leetcode/editor/en/[21]Merge Two Sorted Lists.py
@@ -41,22 +41,40 @@
 
 # leetcode submit region begin(Prohibit modification and deletion)
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        result = temp = ListNode()
+        dummy = result = ListNode()
+        if not (list1 or list2):
+            return list2
+        if not list1:
+            return list2
+        if not list2:
+            return list1
 
         while list1 and list2:
             if list1.val <= list2.val:
-                result.val = list1.val
-                result.next = ListNode()
+                result.next = ListNode(list1.val)
+                list1 = list1.next
             else:
-                result.val = list2.val
+                result.next = ListNode(list2.val)
                 list2 = list2.next
-        return result
+            result = result.next
+
+        while list1:
+            result.next = ListNode(list1.val)
+            list1 = list1.next
+            result = result.next
+
+        while list2:
+            result.next = ListNode(list2.val)
+            list2 = list2.next
+            result = result.next
+
+        return dummy.next
 
 
 #     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
