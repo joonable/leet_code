8617b8a8778499bc42b6f8edd6f806991e8391ea
joon <joonable2@gmail.com>
2025-04-04 10:47:24 +0100

    initial commit

---
+++ leetcode/editor/en/[199]Binary Tree Right Side View.py
@@ -0,0 +1,86 @@
+# Given the root of a binary tree, imagine yourself standing on the right side 
+# of it, return the values of the nodes you can see ordered from top to bottom. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+#  Input: root = [1,2,3,null,5,null,4] 
+#  
+# 
+#  Output: [1,3,4] 
+# 
+#  Explanation: 
+# 
+#  
+# 
+#  Example 2: 
+# 
+#  
+#  Input: root = [1,2,3,4,null,null,null,5] 
+#  
+# 
+#  Output: [1,3,4,5] 
+# 
+#  Explanation: 
+# 
+#  
+# 
+#  Example 3: 
+# 
+#  
+#  Input: root = [1,null,3] 
+#  
+# 
+#  Output: [1,3] 
+# 
+#  Example 4: 
+# 
+#  
+#  Input: root = [] 
+#  
+# 
+#  Output: [] 
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  The number of nodes in the tree is in the range [0, 100]. 
+#  -100 <= Node.val <= 100 
+#  
+# 
+#  Related Topics Tree Depth-First Search Breadth-First Search Binary Tree 👍 12
+# 621 👎 1056
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for a binary tree node.
+from typing import Optional, List
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        queue = [root]
+        result = []
+        while queue:
+            result.append(queue[-1].val)
+            for _ in range(len(queue)):
+                node = queue.pop(0)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return result
+
+
+
+        
+# leetcode submit region end(Prohibit modification and deletion)
